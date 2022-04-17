7d6d678937c37d98df6755d95f21da07568889cd
Lucas <crrluc003@myuct.ac.za>
2022-04-18 01:23:12 +0200

    Implemented OPT algorithm

--- paging.py
+++ paging.py
@@ -16,26 +16,16 @@ def generate_reference_string(number_of_pages):
     return page_array
 
 def find_optimal_element(future_pages, current_queue):
-    values = [current_queue[0], current_queue[1], current_queue[2]]
-    positions = [-1, -1, -1]
-    counter = 0
-    print (future_pages)
-    print (current_queue)
+    values = current_queue
+    positions = []
 
-    for element in future_pages:
+    for value in values:
+        try:
+            positions.append(future_pages.index(value))
+        except:
+            return value
 
-        if element == values[0] and positions[0] == -1:
-            positions[0] = future_pages[counter]
-
-        elif element == values[1] and positions[1] == -1:
-            positions[1] = future_pages[counter]
-
-        elif element == values[2] and positions[2] == -1:
-            positions[2] = future_pages[counter]
-
-        counter += 1
-
-    return (max(positions))
+    return future_pages[max(positions)]
 
 #FIFO Replacement Algorithm
 def FIFO(frame_size, reference_string):
@@ -79,17 +69,19 @@ def OPT(frame_size, reference_string):
     opt_queue = []
     counter = 0
     
-
     for element in reference_string:
+        
         if element not in opt_queue:
-            if len(opt_queue) == 3:
-                element_to_remove = find_optimal_element(reference_string[counter+1:], opt_queue)
+            if len(opt_queue) == frame_size:
+                page_faults += 1
+                element_to_remove = find_optimal_element(reference_string[(counter)+1:], opt_queue)
                 opt_queue.remove(element_to_remove)
                 opt_queue.append(element)
-                page_faults += 1
+                
             else:
-                opt_queue.append(element)
                 page_faults += 1
+                opt_queue.append(element)
+
         counter += 1
         
     return page_faults
@@ -100,6 +92,7 @@ def main():
     size = int(sys.argv[1])                                     #this is the size of the reference string
     number_of_frames = 3                                       
     reference_string = generate_reference_string(size)
+    
 
     print ("FIFO: ", FIFO(number_of_frames, reference_string), " page faults.")
     print ("LRU: ", LRU(number_of_frames, reference_string), " page faults.")
