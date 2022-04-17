967cb14abec3035c43402ec70caee6ca8cff0fd7
Lucas <crrluc003@myuct.ac.za>
2022-04-17 22:59:15 +0200

    Added reference string generator & FIFO algorithm implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 3002-Assignment-3
+<h2>CSC3002F Assignment 3 </h2>
+
+<h3> Lucas Carr	||	CRRLUC003 </h3>
--- paging.py
+++ paging.py
@@ -5,10 +5,30 @@
 
 #libararies
 import sys
+import random as r
 
 #Source Code
+#Helper Functions
+def generate_reference_string(number_of_pages):
+    page_array = [] 
+    for page in range(number_of_pages):
+        page_array.append(r.randrange(0,9))
+    return page_array
 
 #FIFO Replacement Algorithm
+def fifo_replacement(reference_string, frame_size):
+    page_faults = 0
+    fifo_queue = []
+    for element in reference_string:
+        print (element)
+        if element not in fifo_queue:
+            if len(fifo_queue) == frame_size:
+                fifo_queue.pop()
+                fifo_queue.append(element)
+                page_faults += 1
+            else:
+                fifo_queue.append(element)
+    return page_faults
 
 
 #LRU Replacement Algorithm
@@ -19,8 +39,11 @@ import sys
 
 #Main Functionality
 def main():
-    size = int(sys.argv[1])
-    print ("FIFO")
+    size = int(sys.argv[1])                                     #this is the size of the reference string
+    number_of_frames = 3                                       
+    reference_string = generate_reference_string(size)
+    print ("FIFO: ", fifo_replacement(reference_string, number_of_frames), " page faults.")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
