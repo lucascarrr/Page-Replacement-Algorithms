a848b9eec7a5faddcc22cc91e09db5f5ac3202c8
Lucas <crrluc003@myuct.ac.za>
2022-04-18 00:23:29 +0200

    Implemented LRU algorithm

--- paging.py
+++ paging.py
@@ -15,34 +15,95 @@ def generate_reference_string(number_of_pages):
         page_array.append(r.randrange(0,9))
     return page_array
 
+def find_optimal_element(future_pages, current_queue):
+    values = [current_queue[0], current_queue[1], current_queue[2]]
+    positions = [-1, -1, -1]
+    counter = 0
+    print (future_pages)
+    print (current_queue)
+
+    for element in future_pages:
+
+        if element == values[0] and positions[0] == -1:
+            positions[0] = future_pages[counter]
+
+        elif element == values[1] and positions[1] == -1:
+            positions[1] = future_pages[counter]
+
+        elif element == values[2] and positions[2] == -1:
+            positions[2] = future_pages[counter]
+
+        counter += 1
+
+    return (max(positions))
+
 #FIFO Replacement Algorithm
-def fifo_replacement(reference_string, frame_size):
+def FIFO(frame_size, reference_string):
     page_faults = 0
     fifo_queue = []
+
     for element in reference_string:
-        print (element)
         if element not in fifo_queue:
             if len(fifo_queue) == frame_size:
-                fifo_queue.pop()
+                fifo_queue.pop(0)
                 fifo_queue.append(element)
                 page_faults += 1
             else:
                 fifo_queue.append(element)
+                page_faults += 1
+                
     return page_faults
 
-
 #LRU Replacement Algorithm
+def LRU(frame_size, reference_string):
+    page_faults = 0
+    lru_queue = []
 
+    for element in reference_string:
+        if element in lru_queue:
+            lru_queue.remove(element)
+            lru_queue.append(element)
+        elif len(lru_queue) == frame_size:
+            lru_queue.pop(0)
+            lru_queue.append(element)
+            page_faults += 1
+        else:
+            lru_queue.append(element)
+            page_faults += 1
+    
+    return page_faults
 
 #Optimal Replacement Algorithm
+def OPT(frame_size, reference_string):
+    page_faults = 0
+    opt_queue = []
+    counter = 0
+    
 
+    for element in reference_string:
+        if element not in opt_queue:
+            if len(opt_queue) == 3:
+                element_to_remove = find_optimal_element(reference_string[counter+1:], opt_queue)
+                opt_queue.remove(element_to_remove)
+                opt_queue.append(element)
+                page_faults += 1
+            else:
+                opt_queue.append(element)
+                page_faults += 1
+        counter += 1
+        
+    return page_faults
 
+        
 #Main Functionality
 def main():
     size = int(sys.argv[1])                                     #this is the size of the reference string
     number_of_frames = 3                                       
     reference_string = generate_reference_string(size)
-    print ("FIFO: ", fifo_replacement(reference_string, number_of_frames), " page faults.")
+
+    print ("FIFO: ", FIFO(number_of_frames, reference_string), " page faults.")
+    print ("LRU: ", LRU(number_of_frames, reference_string), " page faults.")
+    print ("OPT: ", OPT(number_of_frames, reference_string), " page faults.")
 
 
 if __name__ == "__main__":
