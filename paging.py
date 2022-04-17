ae0c4e7005232a64960a887c28a09c88685a103e
Lucas <crrluc003@myuct.ac.za>
2022-04-17 21:56:29 +0200

    Added skeleton for program

--- paging.py
+++ paging.py
@@ -1,1 +1,29 @@
-def 
+# Source code for CSC3002 Assignment 3
+# Lucas Carr
+# CRRLUC003
+# 17/04/22
+
+#libararies
+import sys
+
+#Source Code
+
+#FIFO Replacement Algorithm
+
+
+#LRU Replacement Algorithm
+
+
+#Optimal Replacement Algorithm
+
+
+#Main Functionality
+def main():
+    size = int(sys.argv[1])
+    print ("FIFO")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print ("Usage: python paging.py [number of pages]")
+    else:
+        main()
